61a33ec38c9795225d7f13c5bb729ae326fe5ac4
leiah133 <leiaanjos87@gmail.com>
2024-11-19 10:47:42 -0300

    Commit Initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+import styles from './Header.module.css'
+
+
+
+export function Header(){
+  return(
+    <header className={styles.header}>
+      <strong>Pink Feed</strong>
+    </header>
+  );
+}
